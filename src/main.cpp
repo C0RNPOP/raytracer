33d3f62afe0e85f337b8ff50f3037db1f3daaac8
Corn Pop <corn.pop.mail@gmail.com>
2022-11-12 22:10:38 +0100

    Putting Vector and Point together in a small main program

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
 
+#include "point.hpp"
+#include "vector.hpp"
+
+struct Projectile
+{
+    Point position;
+    Vector velocity;
+};
+
+struct Environment
+{
+    Vector gravity;
+    Vector wind;
+};
+
+Projectile tick(Projectile proj, Environment env)
+{
+    Point position = proj.position + proj.velocity;
+    Vector velocity = proj.velocity + env.gravity + env.wind;
+
+    return Projectile{ position, velocity };
+}
+
 int main()
 {
-    std::cout << "Hello, world!\n";
+    Projectile proj{ Point{ 0, 1, 0 }, Vector{ 1, 1, 0 }.Normalized() };
+    Environment env{ Vector{ 0, -0.1, 0 }, Vector{ -0.01, 0, 0 } };
+
+    unsigned int turn = 0;
+    while(proj.position.Y() > 0)
+    {
+        std::cout << "Turn " << turn << ": " << proj.position << "\n"; proj = tick(proj, env);
+        turn++;
+    }
 }
