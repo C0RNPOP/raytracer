e49d911395b645d2c2e964f62c879c8431674e7e
Corn Pop <corn.pop.mail@gmail.com>
2022-11-13 17:50:46 +0100

    Implement Canvas to PPM conversion

--- src/canvas.hpp
+++ src/canvas.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
+#include <algorithm>
 #include <cstddef>
+#include <sstream>
 
 #include "color.hpp"
 
@@ -21,9 +23,63 @@ class Canvas
             return data[x + y * width];
         }
 
+        std::string ToPPM() const
+        { 
+            std::ostringstream ppm;
+            WritePPMHeader(ppm);
+            WritePPMBody(ppm);
+            return ppm.str();
+        }
+
         std::size_t Width() const { return width; }
         std::size_t Height() const { return height; }
 
     private:
         std::array<Color, width * height> data;
+
+        void WritePPMHeader(std::ostringstream& ppm) const
+        {
+            ppm << "P3\n";
+            ppm << width << " " << height << "\n";
+            ppm << "255\n";
+        }
+
+        void WritePPMBody(std::ostringstream& ppm) const
+        {
+            for (std::size_t y = 0; y < height; y++)
+            {
+                std::size_t lineLen{ 0 };
+                for(std::size_t x = 0; x < width; x++)
+                {
+                    Color color = (*this)(x, y);
+
+                    WritePPMComponent(ppm, color.R(), lineLen);
+                    WritePPMComponent(ppm, color.G(), lineLen);
+                    WritePPMComponent(ppm, color.B(), lineLen);
+                }
+                ppm << "\n";
+            }
+        }
+
+        void WritePPMComponent(std::ostringstream& ppm, double comp, std::size_t& lineLen) const
+        {
+            constexpr std::size_t PPM_LINE_LIMIT = 70;
+
+            unsigned int clampedComp = std::round(std::clamp(255 * comp, 0.0, 255.0));
+            std::string stringComp = std::to_string(clampedComp);
+            
+            if (lineLen + 1 + stringComp.length() > PPM_LINE_LIMIT)
+            {
+                ppm << "\n";
+                lineLen = 0;
+            }
+            else if (lineLen != 0)
+            {
+                ppm << " ";
+                lineLen += 1;
+            }
+
+            ppm << stringComp;
+            lineLen += stringComp.length();
+        }
 };
--- src/canvas.test.cpp
+++ src/canvas.test.cpp
@@ -1,6 +1,10 @@
 #include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_string.hpp>
 
 #include "canvas.hpp"
+#include "util.hpp"
+
+using Catch::Matchers::EndsWith;
 
 TEST_CASE("Creating a Canvas")
 {
@@ -54,4 +58,61 @@ TEST_CASE("Writing pixels to a Canvas")
 
     REQUIRE(red == canvas(2, 3));
     REQUIRE(red != canvas(3, 3));
+}
+
+TEST_CASE("Converting a Canvas to a PPM")
+{
+    SECTION("Constructing the PPM header")
+    {
+        Canvas<5, 3> canvas{};
+
+        std::vector<std::string> splitPPM = split(canvas.ToPPM(), '\n');
+        std::vector<std::string> expectedPPMHeader{ "P3", "5 3", "255" };
+        std::vector<std::string> actualPPMHeader{ splitPPM.begin(), splitPPM.begin() + 3 };
+
+        REQUIRE(expectedPPMHeader == actualPPMHeader);
+    }
+
+    SECTION("Constructing the PPM Body")
+    {
+        Canvas<5, 3> canvas{};
+        canvas(0, 0) = Color{ 1.5, 0, 0 };
+        canvas(2, 1) = Color{ 0, 0.5, 0 };
+        canvas(4, 2) = Color{ -0.5, 0, 1 };
+
+        std::vector<std::string> splitPPM = split(canvas.ToPPM(), '\n');
+        std::vector<std::string> expectedPPMBody{ 
+            "255 0 0 0 0 0 0 0 0 0 0 0 0 0 0",
+            "0 0 0 0 0 0 0 128 0 0 0 0 0 0 0",
+            "0 0 0 0 0 0 0 0 0 0 0 0 0 0 255",
+        };
+        std::vector<std::string> actualPPMBody{ splitPPM.begin() + 3, splitPPM.begin() + 6 };
+
+        REQUIRE(expectedPPMBody == actualPPMBody);
+    }
+
+    SECTION("Splitting long lines in PPM files")
+    {
+        Canvas<10, 2> canvas{ Color{ 1, 0.8, 0.6 } };
+
+        std::vector<std::string> splitPPM = split(canvas.ToPPM(), '\n');
+        std::vector<std::string> expectedPPMBody{ 
+            "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204",
+            "153 255 204 153 255 204 153 255 204 153 255 204 153",
+            "255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204",
+            "153 255 204 153 255 204 153 255 204 153 255 204 153",
+        };
+        std::vector<std::string> actualPPMBody{ splitPPM.begin() + 3, splitPPM.begin() + 7 };
+
+        REQUIRE(expectedPPMBody == actualPPMBody);
+    }
+
+    SECTION("PPM files are terminated by a newline character")
+    {
+        Canvas<5, 3> canvas{};
+
+        std::string ppm = canvas.ToPPM();
+
+        REQUIRE_THAT(ppm, EndsWith("\n"));
+    }
 }
